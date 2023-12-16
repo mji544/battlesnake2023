f4b87b26cd8b7ef0b49e9b5df1464f750a56de78
Matthew Ingold <matthew.ingold@fcc-fac.ca>
2023-12-16 11:01:11 -0600

    trying path things

--- src/escape.service.ts
+++ src/escape.service.ts
@@ -11,37 +11,60 @@ export class EscapeService {
   constructor(private boardService: BoardService) {}
 
   public checkIfTrapped() {
-    this.gameBoard = this.boardService.board;
+    
   }
 
   public takeEscapeRoute(gameState: GameState) {
-    this.checkIfTrapped();
+    const vicinityBoard = this.getVicinityBoard(gameState);
+    const myHeadCoord = gameState.you.head;
+
+    // Initialize a 2D array to keep track of visited cells
+    const visited: boolean[][] = Array.from({ length: vicinityBoard.length }, () => Array(vicinityBoard[0].length).fill(false));
+
+    // Use DFS to find a continuous, connecting path
+    if (this.dfs(myHeadCoord, vicinityBoard, visited)) {
+      console.log("other", visited, "something", visited.map((row, rowIndex) => row.map((cell, colIndex) => (cell ? vicinityBoard[rowIndex][colIndex] : 0))))
+      return visited.map((row, rowIndex) => row.map((cell, colIndex) => (cell ? vicinityBoard[rowIndex][colIndex] : 0)));
+    } else {
+      return null; // No valid path found
+    }
+  }
+
+  public getVicinityBoard(gameState: GameState): SpaceContains[][] {
+    this.gameBoard = this.boardService.board;
     const myHeadCoord = gameState.you.head;
     //treat everything else as a barrier
     //follow tail FIXME
     //look four square radius
     let vicinity = [...this.gameBoard];
     const indicesToSplice: SplicingIndices = this.indicesToSplice(gameState, myHeadCoord);
-    console.log("indices", indicesToSplice)
-    // splice rows
+
     vicinity = vicinity.splice(gameState.board.height-indicesToSplice.up, indicesToSplice.up-indicesToSplice.down);
     let index = 0;
     for (let row of vicinity) {
-      console.log("before", row)
+      // console.log("before", row)
       row = row.slice(indicesToSplice.left, indicesToSplice.right);
-      console.log("after", row)
+      // console.log("after", row)
       vicinity[index] = row;
       index++;
     }
-
-    console.log(vicinity)//, this.gameBoard)
+    // console.log(vicinity)
+    return vicinity;
   }
 
   private takeLongestRoute(gameState: GameState) {
 
   }
 
+  private followTail(gameState: GameState) {
 
+  }
+
+
+  public findAvailableRoute(gameState: GameState, board: SpaceContains[][]) {
+    const myHeadCoord = gameState.you.head;
+
+  }
 
   private indicesToSplice(gameState: GameState, start: Coord): SplicingIndices {
     let indices: SplicingIndices = {left: 0, right: 0, up: 0, down: 0};
@@ -71,5 +94,31 @@ export class EscapeService {
     }
 
     return indices;
+  }
+
+  private dfs(startingPoint: Coord, vicinityBoard: SpaceContains[][], visited: boolean[][]): boolean {
+    const rows = vicinityBoard.length;
+    const cols = vicinityBoard[0].length;
+    const x = startingPoint.x;
+    const y = startingPoint.y;
+
+    // Check if the current position is within the grid and is an available space
+    if (x < 0 || x >= rows || y < 0 || y >= cols || visited[x][y] || vicinityBoard[x][y] !== SpaceContains.EMPTY || vicinityBoard[x][y] !== SpaceContains.FOOD) {
+      return false;
+    }
+  
+    visited[x][y] = true;
+  
+    // Check adjacent positions
+    if (
+      this.dfs({x: x - 1, y: y}, vicinityBoard, visited) ||
+      this.dfs({x: x + 1, y: y}, vicinityBoard, visited) ||
+      this.dfs({x: x, y: y - 1}, vicinityBoard, visited) ||
+      this.dfs({x: x, y: y + 1}, vicinityBoard, visited)
+    ) {
+      return true;
+    }
+  
+    return false;
   }
 }
